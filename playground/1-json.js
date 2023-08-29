f13b48aa2c10bb8420e252fbf8917998fbf98e20
Daniel Won <wonsy910@gmail.com>
2023-08-28 21:45:07 -0400

    learning about json

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,9 +1,39 @@
 const fs = require("fs");
 
-const book = {
-  title: "Ego is the Enemy",
-  author: "Ryan Holiday",
+// const book = {
+//   title: "Ego is the Enemy",
+//   author: "Ryan Holiday",
+// };
+
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync("1-json.json", bookJSON);
+
+// const dataBuffer = fs.readFileSync("1-json.json");
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+// console.log(data.title);
+
+const myInfo = {
+  name: "Daniel",
+  age: "25",
 };
 
-const bookJSON = JSON.stringify(book);
+const bookJSON = JSON.stringify(myInfo);
 fs.writeFileSync("1-json.json", bookJSON);
+
+const dataBuffer = fs.readFileSync("1-json.json");
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data);
+
+//Solution
+//Load the data in
+// const dataBuffer = fs.readFileSync("1-json.json");
+// const dataJSON = dataBuffer.toString();
+// const user = JSON.parse(dataJSON);
+
+// user.name = "Gunther";
+// user.age = 54;
+
+// const userJSON = JSON.stringify(user);
+// fs.writeFileSync("1-json.json", userJSON);
